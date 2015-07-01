d0229878c6f76d0f476f837af8708da18055e194
Hyounggyu Kim <hgkim10@gmail.com>
2015-07-01 16:20:01 +0900

    Add python script

--- app/main.js
+++ app/main.js
@@ -10,13 +10,23 @@ var mainWindow = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-  if (process.platform != 'darwin')
+  //if (process.platform != 'darwin')
     app.quit();
 });
 
 // This method will be called when atom-shell has done everything
 // initialization and ready for creating browser windows.
 app.on('ready', function() {
+  var py = require('child_process').spawn('python', [__dirname + '/scripts/hello.py'])
+
+  py.stdout.on('data', function(data) {
+    console.log('stdout:' + data);
+  });
+
+  py.stderr.on('data', function(data) {
+    console.log('stderr:' + data);
+  });
+
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
@@ -29,5 +39,7 @@ app.on('ready', function() {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
+    // Kill sub process
+    py.kill('SIGINT');
   });
 });
---
+++ app/scripts/hello.py
@@ -0,0 +1,14 @@
+import sys, time
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    #time.sleep(5)
+    return "Hello World! This is powered by Python backend."
+
+if __name__ == "__main__":
+    print('oh hello')
+    sys.stdout.flush()
+    app.run(host='127.0.0.1', port=5000)
