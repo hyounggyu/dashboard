6fc09c33670c0814cbc4804b2a29f4840e5f20e5
Hyounggyu Kim <hgkim10@gmail.com>
2015-07-27 23:23:24 +0900

    Change layout (Remove Nav)

--- src/app.jsx
+++ src/app.jsx
@@ -3,56 +3,14 @@ var React = require("react");
 var RemoteView = require("./build/remoteview");
 var Monitor = require("./build/monitor");
 
-var Nav = React.createClass({
-  handleClick: function(page) {
-    this.props.onRoute(page);
-    return;
-  },
-  render: function() {
-    return (
-      <nav className="menu">
-        <a className="menu-item" onClick={this.handleClick.bind(null, "index")}>Home</a>
-        <a className="menu-item" onClick={this.handleClick.bind(null, "remoteview")}>RemoteView</a>
-      </nav>
-    )
-  }
-});
-
-var Index = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <p>Hello World!</p>
-      </div>
-    )
-  }
-});
-
 var App = React.createClass({
-  getInitialState: function() {
-    return {route: "index"}
-  },
-  handleRoute: function(page) {
-    this.setState({route: page});
-  },
   render: function() {
-    var Child;
-    switch (this.state.route) {
-      case "index": Child = Index; break;
-      case "remoteview": Child = RemoteView; break;
-      default: Child = Index;
-    }
     return (
       <div className="container">
-        <div className="columns">
-          <div className="one-fifth column">
-            <Nav onRoute={this.handleRoute} />
-            <Monitor />
-          </div>
-          <div className="four-fifths column">
-            <Child />
-          </div>
-        </div>
+        <h2>Monitor</h2>
+        <Monitor />
+        <h2>RemoteView</h2>
+        <RemoteView />
       </div>
     );
   }
