8f29c4ee97aae4c1ff05aef43d429bda1b149926
Hyounggyu Kim <hgkim10@gmail.com>
2015-07-28 15:56:16 +0900

    Add gulp error handling

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,11 @@ var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
 var concat = require("gulp-concat");
 
-gulp.task("default", function () {
+gulp.task("default", function() {
   return gulp.src("src/*.jsx")
     //.pipe(sourcemaps.init())
     .pipe(babel())
+    .on('error', function(err){ console.log(err.message); })
     //.pipe(concat("app.js"))
     //.pipe(sourcemaps.write("."))
     .pipe(gulp.dest("dist"));
