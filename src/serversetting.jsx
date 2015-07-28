3fc9c0523512084e9166ab72798c30770e3b6e9a
Hyounggyu Kim <hgkim10@gmail.com>
2015-07-28 12:37:22 +0900

    Update ServerSetting

--- src/serversetting.jsx
+++ src/serversetting.jsx
@@ -1,6 +1,23 @@
 var ServerSetting = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    this.props.onServerSettingSubmit();
+    return;
+  },
   render: function() {
-    return <p>Hello</p>;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <dl className="form">
+            <dt><label>IP Address</label></dt>
+            <dd><input type="text" id="ipaddr" placeholder="127.0.0.1" /></dd>
+          </dl>
+          <dl className="form">
+            <dd><button className="btn" type="submit">Setup</button></dd>
+          </dl>
+        </form>
+      </div>
+    )
   }
 });
 
