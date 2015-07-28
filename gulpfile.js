cfa9262fe6a98ab823619136065024d776ccb1f7
Hyounggyu Kim <hgkim10@gmail.com>
2015-07-28 20:20:37 +0900

    Build error handling with plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 var gulp = require("gulp");
 var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
+var plumber = require('gulp-plumber');
 var concat = require("gulp-concat");
 
 gulp.task("default", function() {
   return gulp.src("src/*.jsx")
+    .pipe(plumber())
     //.pipe(sourcemaps.init())
     .pipe(babel())
-    .on('error', function(err){ console.log(err.message); })
     //.pipe(concat("app.js"))
     //.pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest("dist"))
+    .on('error', function(err){ console.log(err.message); });
 });
 
 gulp.watch('src/*.jsx', ['default']);
