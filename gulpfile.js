11eb2d5492f2fc741452b5702372851884c3306b
Hyounggyu Kim <hgkim10@gmail.com>
2015-07-28 20:23:21 +0900

    Remove gulp-sourcemaps and gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,12 @@
-var gulp = require("gulp");
-var sourcemaps = require("gulp-sourcemaps");
-var babel = require("gulp-babel");
+var gulp = require('gulp');
+var babel = require('gulp-babel');
 var plumber = require('gulp-plumber');
-var concat = require("gulp-concat");
 
-gulp.task("default", function() {
-  return gulp.src("src/*.jsx")
+gulp.task('default', function() {
+  return gulp.src('src/*.jsx')
     .pipe(plumber())
-    //.pipe(sourcemaps.init())
     .pipe(babel())
-    //.pipe(concat("app.js"))
-    //.pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist"))
+    .pipe(gulp.dest('dist'))
     .on('error', function(err){ console.log(err.message); });
 });
 
