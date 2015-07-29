8e9e82da6cedff6fa0af8bbbf8c06b3e6a9c5e61
Hyounggyu Kim <hgkim10@gmail.com>
2015-07-29 23:01:13 +0900

    Update RemoteView

--- src/remoteview.jsx
+++ src/remoteview.jsx
@@ -3,64 +3,46 @@ import child_process from 'child_process';
 import React from 'react';
 import {Input, ButtonInput, Panel} from 'react-bootstrap';
 
-class Terminal extends React.Component {
-  render() {
-    return (
-      <div className="terminal">
-        <pre className="terminal">{this.props.data}</pre>
-      </div>
-    );
-  }
-}
-
-class RemoteViewForm extends React.Component {
-  handleSubmit(e) {
-    e.preventDefault();
-    this.props.onRemoteViewSubmit();
-    return;
-  }
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <Input type="test" ref="step" label="Step" placeholder="10" />
-        <ButtonInput value="Run" onClick={this.handleSubmit} />
-      </form>
-    );
-  }
-}
-
 export class RemoteView extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {data: ""};
+    this.state = {output: ""};
   }
-  handleRemoteViewSubmit() {
-    this.setState({data: ""});
-    run = child_process.spawn("python", ["-u", "-m", "xni.manage", "remoteview", "--ip", this.props.hostname]);
+  handleRemoteView(e) {
+    this.setState({output: ""});
+    let ip = this.props.hostname;
+    let step = this.refs.step.getValue();
+    step = step == "" ? "10" : step; // TODO: default is 10?
+    let run = child_process.spawn("python", ["-u", "-m", "xni.manage", "remoteview", "--ip="+ip, "--step="+step]);
+    // TODO: form disable
     //run = child_process.spawn("python", ["-u", __dirname+"/../scripts/hello.py"]);
     run.stdout.on("data", (data) => {
-      //process.stdout.write(""+data);
       this.setState((previousState, currentProps) => {
-        return {data: previousState.data + "" + data};
+        return {output: previousState.output + "" + data};
       });
     }.bind(this));
     run.stderr.on("data", (data) => {
-      //process.stderr.write(""+data);
       this.setState((previousState, currentProps) => {
-        return {data: previousState.data + "" + data};
+        return {output: previousState.output + "" + data};
       });
     }.bind(this));
     run.on("close", (code) => {
       this.setState((previousState, currentProps) => {
-        return {data: previousState.data + "child process exited with code " + code};
+        return {output: previousState.output + "child process exited with code " + code};
       });
+      // TODO: form enable
     }.bind(this));
   }
   render() {
     return (
       <div>
-        <RemoteViewForm onRemoteViewSubmit={this.handleRemoteViewSubmit} />
-        <Terminal data={this.state.data} />
+        <form>
+          <Input type="text" ref="step" label="Step" placeholder="10" />
+          <ButtonInput value="Run" onClick={(e) => this.handleRemoteView(e)} />
+        </form>
+        <Panel>
+          <p>{this.state.output}</p>
+        </Panel>
       </div>
     );
   }
